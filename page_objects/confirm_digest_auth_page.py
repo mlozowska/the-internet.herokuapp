83cb7c64ece407b22ecf2a53784e5c0c871ddd50
Magda_Lena <60215258+mlozowska@users.noreply.github.com>
2021-07-13 21:03:02 +0200

    Update confirm_digest_auth_page.py

--- page_objects/confirm_digest_auth_page.py
+++ page_objects/confirm_digest_auth_page.py
@@ -12,14 +12,3 @@ def confirmation_visible(driver_instance):
         return True
     else:
         return False
-
-
-# def send_email(driver_instance):
-#     wait_for_visibility_of_element_id(driver_instance, email_field)
-#     email_address = driver_instance.find_element_by_id(email_field)
-#     email_address.send_keys('sxk69578@zwoho.com')
-#     value = 'sxk69578@zwoho.com'
-#     if value == email_address.get_attribute("value"):
-#         return True
-#     else:
-#         return False
